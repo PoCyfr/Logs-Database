304288c0ff1264247facee44beedecb12e5cd82b
Rafael Poças <41762896+PoCyfr@users.noreply.github.com>
2019-09-21 02:27:20 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Logs-Database
+
+http://logs.us-east-2.elasticbeanstalk.com/
+
+Create virtual environment and install requirements
+	
+    
+1.      $ virtualenv env-name
+2.      $ source env-name/bin/activate
+3.      $ pip install -r requirements.txt
+4.      $ python3 application.py
+ 
+
+
+    
+    
--- application/models.py
+++ application/models.py
@@ -1,28 +1,5 @@
 from application import db
+import config, application
 
-class Data(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    notes = db.Column(db.String(128), index=True, unique=False)
-    
-    def __init__(self, notes):
-        self.notes = notes
+#empty because database and tables were created manually using sql workbench
 
-    def __repr__(self):
-        return '<Data %r>' % self.notes
-
-class message(db.Model):
-    f1 = db.Column(db.Float)
-    f2 = db.Column(db.Integer)
-    f3 = db.Column(db.Integer)
-    f4 = db.Column(db.Integer)
-    f5 = db.Column(db.Integer)
-    f6 = db.Column(db.Integer)
-    f7 = db.Column(db.Integer)
-    f8 = db.Column(db.Integer)
-    f9 = db.Column(db.Integer)
-    f10 = db.Column(db.Integer)
-    f11 = db.Column(db.Integer)
-    f12 = db.Column(db.Integer)
-    f13 = db.Column(db.Integer)
-    f14 = db.Column(db.Integer)
-    ID = db.Column(db.Integer,  primary_key=True)
